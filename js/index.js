21c780fadd41b966409fa3383808e3b18a6376ce
iamantha <iamantha@bol.com>
2022-05-24 16:42:57 +0200

    Updates

--- js/index.js
+++ js/index.js
@@ -15,51 +15,15 @@ const alert = (message, type) => {
     $("#alertPlaceholder").html(wrapper)
 }
 
-$("button").click(function () {
-    event.preventDefault();
-    // Disable button and activate loader
-    $("button").prop("disabled", true);
-    $(this).html(
-        `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`
-    );
-
-    let source = $(this).val();
-    let name = $('#userName').val()
-    let modalSource = 'API';
-    $("#ftpImages").hide();
-    $("#apiImages").show();
-
-    if (source == "ftp") {
-        modalSource = 'FTP Server'
-        $("#ftpImages").show();
-        $("#apiImages").hide();
-    }
-
-    $("#modalUsername").html(name);
-    $("#modalSource").html(modalSource);
-
-    $.ajax({
-        type: "POST",
-        url: "https://fair-bond.herokuapp.com/api/flow/session",
-        data: JSON.stringify({name: name, source: source.toUpperCase()}),
-        contentType: "application/json",
-        encode: true,
-    }).done(function (data) {
-        $("#ftpModal").modal('show');
-        console.log(data);
-        // set the item in localStorage
-        localStorage.setItem('session', JSON.stringify(data));
-    }).fail(function (jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR.responseJSON.errorCode)
-        alert(jqXHR.responseJSON.errorText, 'danger')
-    })
-});
-
-function AutoRefresh( t ) {
+function AutoRefresh(t) {
     setTimeout("location.reload(true);", t);
 }
 
 $(document).ready(function () {
+
+    $("#uploadedImage").hide();
+    $("#uploadImageText").hide();
+
     $.ajax({
         type: "GET",
         url: "https://fair-bond.herokuapp.com/api/market/random/text",
@@ -78,6 +42,11 @@ $(document).ready(function () {
         contentType: "application/json",
     }).done(function (data) {
         var image = 'data:image/png;base64,' + data.content;
+        $("#uploadedImage").show();
+        $("#uploadImageText").show();
+        $("#spinner").hide();
+        $("#spinnerText").hide();
+
         $("#uploadedImage").attr("src", image);
         $("#imageUploadedBy").html(data.uploadedBy);
     }).fail(function (jqXHR, textStatus, errorThrown) {
